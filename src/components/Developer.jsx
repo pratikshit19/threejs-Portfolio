761e0000202e7c58d9a5af98adb1cb65b3a3b5a3
pratikshit19 <pratikshit@icloud.com>
2025-11-03 02:18:39 +0530

    New Character

--- src/components/Developer.jsx
+++ src/components/Developer.jsx
@@ -12,7 +12,7 @@ import { SkeletonUtils } from 'three-stdlib';
 const Developer = ({ animationName = 'idle', ...props }) => {
   const group = useRef();
 
-  const { scene } = useGLTF('/models/animations/developer.glb');
+  const { scene } = useGLTF('/models/animations/mycharacter.glb');
   const clone = React.useMemo(() => SkeletonUtils.clone(scene), [scene]);
   const { nodes, materials } = useGraph(clone);
 
